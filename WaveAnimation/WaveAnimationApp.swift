106ecae235e8f467331ccd932ad0e03d127754e6
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-04-15 07:09:34 +0900

    Initial Commit

---
+++ WaveAnimation/WaveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAnimationApp.swift
+//  WaveAnimation
+//
+//  Created by ミズキ on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct WaveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
